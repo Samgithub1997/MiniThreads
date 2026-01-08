55eb1406b67f1ae5dc6f5489bd1ba2e9fbf2fdae
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-07 21:36:56 -0800

    chore: Adds posts schema.

--- src/db/schemas/posts.ts
+++ src/db/schemas/posts.ts
@@ -1,3 +1,27 @@
-import { pgTable } from "drizzle-orm/pg-core";
+import { usersTable } from "./users";
+import {
+  pgTable,
+  bigserial,
+  varchar,
+  text,
+  timestamp,
+  boolean,
+  index,
+} from "drizzle-orm/pg-core";
 
-export const postsTable = pgTable("posts", {});
+export const postsTable = pgTable(
+  "posts",
+  {
+    id: bigserial("id", { mode: "bigint" }).primaryKey(),
+    authorName: varchar("author_name").references(() => usersTable.username, {
+      onDelete: "cascade",
+    }),
+    content: text("content").notNull(),
+    isEdited: boolean("is_edited").default(false),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+  },
+  (table) => ({
+    authorNameIdx: index("author_name_idx").on(table.authorName),
+  })
+);
