e2b754d0e2681daaa59573054c965d01a68941e5
Samarth Sharma <samarthsharma351@gmail.com>
2026-01-08 06:49:52 -0800

    chore: Adds follower followee db schema.

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,4 @@
 export * from "./schemas/users";
 export * from "./schemas/comments";
 export * from "./schemas/posts";
+export * from "./schemas/follower-followee";
---
+++ src/db/schemas/follower-followee.ts
@@ -0,0 +1,25 @@
+import { pgTable } from "drizzle-orm/pg-core";
+import { usersTable } from "./users";
+import { bigint, primaryKey, index, timestamp } from "drizzle-orm/pg-core";
+
+export const followersTable = pgTable(
+  "followers-followee",
+  {
+    followerId: bigint("follower_id", { mode: "bigint" })
+      .notNull()
+      .references(() => usersTable.id, {
+        onDelete: "cascade",
+      }),
+    followeeId: bigint("followee_id", { mode: "bigint" })
+      .notNull()
+      .references(() => usersTable.id, {
+        onDelete: "cascade",
+      }),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.followerId, table.followeeId] }),
+    followerIdx: index("follower_idx").on(table.followerId),
+    followeeIdx: index("followee_idx").on(table.followeeId),
+  })
+);
